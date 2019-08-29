66c8ba4611d140cc5073cfc01c2c823de4861b30
kclemons <kclemons@autobase.net>
2019-08-29 16:08:10 -0400

    win screen

---
+++ World/Win.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _ready():
+	if GLOBAL.MUSIC:
+		$Music.play()
+	$Transition/MoneyLabel.set_text("You earned: $" + String(GLOBAL.LOSE_CASH))
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if !GLOBAL.MUSIC:
+		$Music.stop()
+	elif GLOBAL.MUSIC && !$Music.is_playing():
+		$Music.play()
+	if Input.is_action_pressed("interact") && $ContinueTImer.is_stopped():
+		GLOBAL.RESET = true
+		get_tree().change_scene("res://World/MainMenu.tscn")
